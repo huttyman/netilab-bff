f55cfc0a3b570dbb112152dc997bf18a92019e28
huttyman <tossapong.p@gmail.com>
2024-05-24 20:40:31 +0700

    Mock api asknetilab (#2)
    * mock example
    
    * add asknetilabApi
    
    * add mock
    
    ---------
    
    Co-authored-by: tossapong nuntanart <tosssapong.n@appman.co.th>

--- server.js
+++ server.js
@@ -3,15 +3,18 @@ const app = express();
 const port = 3000;
 
 const apiRoutes = require('./routes');
+const asknetilabRoutes = require('./routes/asknetilabRoutes');
 const setupSwagger = require('./swagger');
 
 app.use(express.json());
+// Use routes
 app.use('/api', apiRoutes);
+app.use('/asknetilab', asknetilabRoutes);
 
 setupSwagger(app);
 
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send('Hello World! 1.4');
 });
 
 app.listen(port, () => {
