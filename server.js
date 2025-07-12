df3e4accc5733cffdbc61be7a5a5788330bea1a1
tossapong <tossapong.n@appman.co.th>
2025-07-12 17:52:07 +0700

    fix domain

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/query-netilab-ai', queryNetilabAiRoutes);
 setupSwagger(app);
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 2.0');
+    res.send('Hello World! 2.1');
 });
 
 app.listen(port, () => {
