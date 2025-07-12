fefe4276b7258419d0081df99ce3c34c8428f8ba
tossapong <tossapong.n@appman.co.th>
2025-07-12 23:07:00 +0700

    assign db

--- services/db.js
+++ services/db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host: process.env.PGHOST || 'localhost',
   port: process.env.PGPORT || 5432,
-  user: process.env.PGUSER || 'postgres',
-  password: process.env.PGPASSWORD || '',
-  database: process.env.PGDATABASE || 'postgres',
+  user: process.env.PGUSER || 'netilabuser',
+  password: process.env.PGPASSWORD || 'yourpassword',
+  database: process.env.PGDATABASE || 'netilabdb',
 });
 
 module.exports = pool;
