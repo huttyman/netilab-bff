5f7aaf8cfebc7f158be0414e66e1beea018285e5
huttyman <tossapong.p@gmail.com>
2025-07-14 16:48:10 +0700

    Add DB logging for query endpoints (#18)
    * feat: log query-netilab requests
    
    * assign db
    
    * add package lock
    
    * change db host
    
    * add version
    
    ---------
    
    Co-authored-by: tossapong <tossapong.n@appman.co.th>

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use('/query-netilab-ai', queryNetilabAiRoutes);
 setupSwagger(app);
 
 app.get('/', (req, res) => {
-    res.send('Hello World! 2.4');
+    res.send('Hello World! 2.5');
 });
 
 app.listen(port, () => {
